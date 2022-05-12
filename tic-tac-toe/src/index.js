0e52c14f21e7cd72a51506dae7eb074ad515cb53
TsurDrori <tsurdrori@gmail.com>
2022-05-12 11:00:01 +0300

    first commit

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,8 @@
+import './index.css'; 
+import ReactDOM from 'react-dom/client';
+import { Game } from './cmps/Game';
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
